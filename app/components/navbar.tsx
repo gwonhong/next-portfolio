1ad9575bb7a5163cff76cac6fde4e4b743e72410
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-04 18:32:45 +0900

    UPDATE navbar highlight active item

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,30 +1,39 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 // function Logo() {
 //   return <Image src="/logo.png" alt="logo" fill />;
 // }
 
-function NavItem({ text, href }: { text: string; href: string }) {
-  return <Link href={href}>{text}</Link>;
+function NavItem({ text, path }: { text: string; path: string }) {
+  let pathname = usePathname() || "/";
+  const isActive = path === pathname;
+  return (
+    <Link href={path} className={isActive ? "text-black" : "text-white"}>
+      {text}
+    </Link>
+  );
 }
 export default function Navbar() {
   return (
     <div className="w-full h-20 bg-sky-600 sticky top-0">
       <div className="container mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
-          <ul className="hidden md:flex gap-x-6 text-white">
+          <ul className="hidden md:flex gap-x-6">
             <li>
-              <NavItem text="Home" href="/" />
+              <NavItem text="Home" path="/" />
             </li>
             <li>
-              <NavItem text="About" href="/about" />
+              <NavItem text="About" path="/about" />
             </li>
             <li>
-              <NavItem text="Gallery" href="/gallery" />
+              <NavItem text="Gallery" path="/gallery" />
             </li>
             <li>
-              <NavItem text="Chart" href="/chart" />
+              <NavItem text="Chart" path="/chart" />
             </li>
           </ul>
         </div>
