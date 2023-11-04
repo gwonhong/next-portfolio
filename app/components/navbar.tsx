f90327b52df46f828ee7da4a64e23644923fb010
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-04 17:43:01 +0900

    UPDATE navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,34 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
 
-function Logo() {
-  return <></>;
-}
+// function Logo() {
+//   return <Image src="/logo.png" alt="logo" fill />;
+// }
 
 function NavItem({ text, href }: { text: string; href: string }) {
-  return (
-    <Link href={href}>
-      <button type="button">{text}</button>
-    </Link>
-  );
+  return <Link href={href}>{text}</Link>;
 }
 export default function Navbar() {
   return (
-    <div className="w-full h-20 bg-emerald-800 sticky top-0">
+    <div className="w-full h-20 bg-sky-600 sticky top-0">
       <div className="container mx-auto px-4 h-full">
         <div className="flex justify-between items-center h-full">
-          <Logo />
           <ul className="hidden md:flex gap-x-6 text-white">
             <li>
-              <NavItem text="home" href="/" />
+              <NavItem text="Home" href="/" />
             </li>
             <li>
-              <NavItem text="about" href="/about" />
+              <NavItem text="About" href="/about" />
             </li>
             <li>
-              <NavItem text="gallery" href="/gallery" />
+              <NavItem text="Gallery" href="/gallery" />
             </li>
             <li>
-              <NavItem text="chart" href="/chart" />
+              <NavItem text="Chart" href="/chart" />
             </li>
           </ul>
         </div>
