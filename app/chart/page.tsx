c8c2bf76a2711306d54c72973aaee287b23207b4
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-04 17:43:30 +0900

    ADD about chart gallery routes

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+"use client";
+
+export default function Chart() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>hello</div>
+      <div>chart</div>
     </main>
   );
 }
