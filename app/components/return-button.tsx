f155ff9a0f6eb346771ce85c6daad25e96190aa7
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-05 23:32:19 +0900

    UPDATE return-button

--- app/components/return-button.tsx
+++ app/components/return-button.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
 export default function ReturnButton({ href }: { href: string }) {
   return (
-    <Link
-      href={href}
-      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-    >
-      Return
+    <Link href={href}>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        Return
+      </button>
     </Link>
   );
 }
