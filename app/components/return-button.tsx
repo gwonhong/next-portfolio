edc9b4578aaa12c14995b903669b00fe3aee6ac2
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-07 00:27:30 +0900

    UPDATE return button to have bottom margin

--- app/components/return-button.tsx
+++ app/components/return-button.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function ReturnButton({ href }: { href: string }) {
   return (
     <Link href={href}>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 mb-6 border border-blue-500 hover:border-transparent rounded">
         Return
       </button>
     </Link>
