9aa4fbf27ca89e7b9dd41ba1fe2e99f2412ed841
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-07 01:26:05 +0900

    UPDATE simpler spinner component

--- app/projects/[title]/loading.tsx
+++ app/projects/[title]/loading.tsx
@@ -1,22 +1,9 @@
-"use client";
-
-import { CirclesWithBar } from "react-loader-spinner";
+import Spinner from "@/app/components/spinner";
 
 export default function Loading(): JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center">
-      <CirclesWithBar
-        height="150"
-        width="150"
-        color="#4fa94d"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        outerCircleColor=""
-        innerCircleColor=""
-        barColor=""
-        ariaLabel="circles-with-bar-loading"
-      />
+      <Spinner />
     </div>
   );
 }
