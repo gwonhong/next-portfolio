956b3ebe87b2e521eae0e6441053bfa186e1ade5
Gwon Hong <gwonhong.dev@gmail.com>
2023-11-06 15:35:38 +0900

    UPDATE chart page

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 function validateYears(start: number, end: number) {
   if (start < 1960 || start > 2022) return false;
   if (end < 1960 || end > 2022) return false;
+  if (start > end) return false;
   return true;
 }
 
@@ -15,15 +16,16 @@ export default function ChartPage() {
   const [end, setEnd] = useState(2022);
   const [isWrong, setIsWrong] = useState(false);
   return (
-    <div>
-      <h1>Line Chart about Korea&apos;s population change</h1>
-      <h3 className={isWrong ? "text-red" : ""}>
+    <div className="prose">
+      <h2>Line Chart about Korea&apos;s population change</h2>
+      <h3 className={isWrong ? "text-red-600" : ""}>
         You should input years between 1960-2022.
       </h3>
       <p>
-        Start Year:{" "}
+        <label>Start Year: </label>
         <input
           type="number"
+          className="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded"
           value={start}
           onChange={(e) => {
             setStart(parseInt(e.target.value));
@@ -31,9 +33,10 @@ export default function ChartPage() {
         ></input>
       </p>
       <p>
-        End Year:{" "}
+        <label>End Year: </label>
         <input
           type="number"
+          className="bg-transparent text-blue-700 font-semibold py-2 px-4 border border-blue-500 rounded"
           value={end}
           onChange={(e) => {
             setEnd(parseInt(e.target.value));
@@ -42,6 +45,7 @@ export default function ChartPage() {
       </p>
       <button
         type="button"
+        className="bg-blue-500 hover:bg-transparent text-white font-semibold hover:text-blue-700 py-2 px-4 border border-transparent hover:border-blue-500 rounded"
         onClick={() => {
           const isValid = validateYears(start, end);
           if (isValid) router.push(`/line-chart/${start}-${end}`);
